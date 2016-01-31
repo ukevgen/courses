df98076ffb3aaf69b07547b21595bc1c180d3925
Ievgen Tararaka <ievgentararaka@gmail.com>
2016-01-31 15:32:48 +0200

    Add separate folders for topics. Init 1st and 2nd topics

---
+++ topic2/src/java/test/com/rxn1d/courses/HomeWorkTest.java
@@ -0,0 +1,18 @@
+package com.rxn1d.courses;
+
+/**
+ * Это файл должен остаться неизменным!
+ *
+ * @author Ievgen Tararaka
+ */
+public class HomeWorkTest {
+    private HomeWork homeWork;
+    private int[][] intArrays;
+    private double[][] doubleArrays;
+    private short[][] shortArrays;
+    private char[][] charArrays;
+
+    public void setUp() {
+        homeWork = new HomeWork();
+    }
+}
