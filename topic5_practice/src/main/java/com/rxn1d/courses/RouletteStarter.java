5de3303f96f06e0c10cc3ed2f79d13c25506cc3b
Ievgen Tararaka <ievgentararaka@gmail.com>
2016-02-24 15:18:45 +0200

    Add start class and artifact information

---
+++ topic5_practice/src/main/java/com/rxn1d/courses/RouletteStarter.java
@@ -0,0 +1,13 @@
+package com.rxn1d.courses;
+
+/**
+ * Входная точка в игру
+ *
+ * @author Ievgen Tararaka
+ */
+public class RouletteStarter {
+
+    public static void main(String[] args) {
+        System.out.println("Roulette start");
+    }
+}
