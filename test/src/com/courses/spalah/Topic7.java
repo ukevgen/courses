9b13f843906a2d041dfc05af082fcee42d12de11
Ievgen Tararaka <ievgentararaka@gmail.com>
2016-03-02 00:32:59 +0200

    Add module 7 draft

---
+++ test/src/com/courses/spalah/Topic7.java
@@ -0,0 +1,17 @@
+package com.courses.spalah;
+
+/**
+ * @author Ievgen Tararaka
+ */
+public class Topic7 {
+    public static void test() {
+        // 1) Почему мы используем коллекции
+
+    }
+
+    public static void whyCollections() {
+        int[] array = {1, 2, 3, 4};
+
+
+    }
+}
