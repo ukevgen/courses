9efb2eb382534fffbb4d245f02b5291975d8a4aa
Ievgen Tararaka <ievgentararaka@gmail.com>
2016-03-03 21:51:59 +0200

    Finilize first home work task of topic 7

--- topic7/src/main/java/com/courses/spalah/list/Car.java
+++ topic7/src/main/java/com/courses/spalah/list/Car.java
@@ -10,9 +10,6 @@ public class Car {
     private int priceInDollars;
     private String modelName;
 
-    public Car() {
-    }
-
     public Car(int priceInDollars, String modelName) {
         this.priceInDollars = priceInDollars;
         this.modelName = modelName;
@@ -32,24 +29,5 @@ public class Car {
 
     public void setModelName(String modelName) {
         this.modelName = modelName;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Car car = (Car) o;
-
-        if (priceInDollars != car.priceInDollars) return false;
-        return modelName.equals(car.modelName);
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = priceInDollars;
-        result = 31 * result + modelName.hashCode();
-        return result;
     }
 }
--- topic7/src/main/java/com/courses/spalah/list/ListFactory.java
+++ topic7/src/main/java/com/courses/spalah/list/ListFactory.java
@@ -1,65 +1,14 @@
 package com.courses.spalah.list;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-
 /**
  * @author Ievgen Tararaka
  */
 public class ListFactory {
-    public static <E> com.courses.spalah.list.MyList<E> createList(ListType listType) {
-        return new MyMyList();
-    }
-
-    public static class MyMyList<E> implements com.courses.spalah.list.MyList<E> {
-        java.util.List<E> list = new LinkedList<>();
-
-        public MyMyList() {
+    public static <E> MyList<E> createList(ListType listType) {
+        if (listType == ListType.SINGLY_LINKED_LIST) {
+            // возвращаем вашу имплементацию односвязного списка
+            return null;
         }
-
-        @Override
-        public int size() {
-            return list.size();
-        }
-
-        @Override
-        public boolean isEmpty() {
-            return list.isEmpty();
-        }
-
-        @Override
-        public boolean add(E element) {
-            return list.add(element);
-        }
-
-        @Override
-        public void add(int index, E element) {
-            list.add(index, element);
-        }
-
-        @Override
-        public void remove(int index) {
-            list.remove(index);
-        }
-
-        @Override
-        public E get(int index) {
-            return list.get(index);
-        }
-
-        @Override
-        public E set(int index, E element) {
-            return list.set(index, element);
-        }
-
-        @Override
-        public boolean contains(E element) {
-            return list.contains(element);
-        }
-
-        @Override
-        public Iterator<E> iterator() {
-            return list.iterator();
-        }
+        return null;
     }
 }
