a446e27b888223ee1d790131f8ad3ba21c1a8a7a
Ievgen Tararaka <ievgentararaka@gmail.com>
2016-03-04 14:52:59 +0200

    Add task for text analyzing

---
+++ topic8/src/main/java/com/courses/spalah/text/analyzer/TextAnalyzer.java
@@ -0,0 +1,10 @@
+package com.courses.spalah.text.analyzer;
+
+/**
+ * @author Ievgen Tararaka
+ */
+public class TextAnalyzer {
+    public static void main(String[] args) {
+        // Входная точка для аналайзера текста
+    }
+}
