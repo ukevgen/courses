0e0d100cbe1cdd9d6cc1dde8d4e36f9ab3f6cb98
ukevgen <ukevgen@gmail.com>
2016-03-29 07:22:57 +0300

    change

--- topic12/src/main/java/Test.java
+++ topic12/src/main/java/Test.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -12,18 +15,23 @@ public class Test {
         try {
             Dbo dbo = new Dbo("localhost:3306","person", "root", "");
             dbo.dboConnection();
-            dbo.findCars();
-            //ResultSet rs = dbo.makeSelect(dbo.selectAll());
+            //dbo.findCars();
+            ResultSet rs = dbo.makeSelect(dbo.selectAll());
             DatabaseTableModel dbm = new DatabaseTableModel(false);
             JTable table = new JTable(dbm);
+            dbm.setDataSource(rs);
+            ButtonColumn buttonColumn = new ButtonColumn(table,6);
+            buttonColumn.setMnemonic(KeyEvent.VK_D);
+
             JFrame frame = new JFrame("INFO");
             frame.setSize(400, 300);
             frame.getContentPane().add(new JScrollPane(table));
             frame.show();
-            //dbm.setDataSource(rs);
+
             //rs.close();
-            ResultSet rs=dbo.findCars();
-            dbm.setDataSource(rs);
+            //ResultSet rs=dbo.findCars();
+            //dbm.setDataSource(rs);
+
             dbo.closeConnection();
         } catch (Exception ex) {
             ex.printStackTrace();
