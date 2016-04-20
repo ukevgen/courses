60700f3bfe549e55e04c3f5ea5a7c87c15485ca9
Ievgen Tararaka <ievgentararaka@gmail.com>
2016-04-20 17:52:50 +0300

    Add wait\notify example

--- topic20/src/com/courses/spalah/ThreadCommunication.java
+++ topic20/src/com/courses/spalah/ThreadCommunication.java
@@ -4,8 +4,21 @@ package com.courses.spalah;
  * @author Ievgen Tararaka
  */
 public class ThreadCommunication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        waitNotify();
+    }
 
+    public static void waitNotify() throws InterruptedException {
+        Object obj = new Object();
+        Thread thread = new Thread(new WaitingWorker(obj), "worker#1");
+        thread.start();
+
+        System.out.println("MAIN THREAD SLEEP FOR 1 SEC");
+        Thread.sleep(1000);
+
+        synchronized (obj) {
+            obj.notify();
+        }
     }
 }
 
@@ -28,6 +41,27 @@ class SignalWorker implements Runnable {
     public void run() {
         while (!signal.isWorkDone()) {
             // ничего не делаем, но расходуем процессорное время =(
+        }
+    }
+}
+
+class WaitingWorker implements Runnable {
+    private Object object;
+
+    public WaitingWorker(Object object) {
+        this.object = object;
+    }
+
+    @Override
+    public void run() {
+        synchronized (object) {
+            System.out.println(Thread.currentThread().getName() + " WAITING");
+            try {
+                object.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("DO MY WORK");
         }
     }
 }
