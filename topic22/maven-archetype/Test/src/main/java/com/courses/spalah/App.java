8303fd6fcd33f5566c61ce53c50cac17795903fc
Ievgen Tararaka <ievgentararaka@gmail.com>
2016-05-03 15:26:09 +0300

    Add more maven examples

---
+++ topic22/maven-archetype/Test/src/main/java/com/courses/spalah/App.java
@@ -0,0 +1,13 @@
+package com.courses.spalah;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
