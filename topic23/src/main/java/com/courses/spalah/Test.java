840b5bea0af4e5b2a14a874424104f08ffc1e29b
Ievgen Tararaka <ievgentararaka@gmail.com>
2016-05-10 08:18:57 +0300

    Add topic 23 draft

---
+++ topic23/src/main/java/com/courses/spalah/Test.java
@@ -0,0 +1,11 @@
+package com.courses.spalah;
+
+import javax.xml.transform.Result;
+
+/**
+ * @author Ievgen Tararaka
+ */
+public class Test {
+    public static void main(String[] args) {
+    }
+}
