5452502e07777cf5bbe6279a72d51a2ccab2e335
Ievgen Tararaka <ievgentararaka@gmail.com>
2016-05-25 18:30:49 +0300

    Add some more files

--- topic26/src/main/java/com/courses/spalah/ExampleController.java
+++ topic26/src/main/java/com/courses/spalah/ExampleController.java
@@ -1,7 +1,9 @@
 package com.courses.spalah;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author Ievgen Tararaka
@@ -10,7 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ExampleController {
 
     @RequestMapping("/say_hello")
-    public String hello() {
+    public String hello(Model model) {
         return "jsp/hello";
+    }
+
+    @RequestMapping("/say_hello_body")
+    @ResponseBody
+    public String helloBody() {
+        return "hello";
     }
 }
